7a67fb2a0d33cf8dfa75c251d5d627c91659a18e
Gregory Fan <gfan1316@gmail.com>
2026-02-19 18:35:20 -0500

    comments

--- request_test.py
+++ request_test.py
@@ -1,5 +1,9 @@
 import requests
 
+# Get's the number of deaths by industry type.
 req = requests.get('https://api.fda.gov/food/event.json?search=reactions.exact:"Death"&count=products.industry_name.exact')
+
+#Prints the response text in the form of JSON.
 req_text = req.content.decode('utf-8')
 print(req_text)
+
