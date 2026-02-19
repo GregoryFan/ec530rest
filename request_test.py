7f41e985de6cafc80bef9c64b8abce63410ed590
Gregory Fan <gfan1316@gmail.com>
2026-02-19 18:33:49 -0500

    req change

--- request_test.py
+++ request_test.py
@@ -1,5 +1,5 @@
 import requests
 
-req = requests.get('https://api.fda.gov/food/event.json?search=products.name_brand:"nuts"&count=reactions.exact')
+req = requests.get('https://api.fda.gov/food/event.json?search=reactions.exact:"Death"&count=products.industry_name.exact')
 req_text = req.content.decode('utf-8')
 print(req_text)
